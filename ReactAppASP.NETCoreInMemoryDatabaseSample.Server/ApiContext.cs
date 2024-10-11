0e5d32f9cae5c85f9f82d7cc36c3aec2aa95369e
Michael Ferguson <mikeygsxr1k@gmail.com>
2024-10-11 18:51:51 +0200

    Created WeatherRepository class that implements the members of the IWeatherRepository interface.

---
+++ ReactAppASP.NETCoreInMemoryDatabaseSample.Server/ApiContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ReactAppASP.NETCoreInMemoryDatabaseSample.Server
+{
+    public class ApiContext : DbContext
+    {
+        protected override void OnConfiguring
+       (DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase(databaseName: "weatherforecasts");
+        }
+        public DbSet<WeatherForecast> WeatherForecasts { get; set; }
+    }
+}
---
+++ ReactAppASP.NETCoreInMemoryDatabaseSample.Server/WeatherRepository.cs
@@ -0,0 +1,40 @@
+﻿namespace ReactAppASP.NETCoreInMemoryDatabaseSample.Server
+{
+    public class WeatherRepository : IWeatherRepository
+    {
+        private static readonly string[] Summaries = new[]
+{
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        public WeatherRepository() 
+        {
+            var weatherList = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            }).ToList();
+
+            using (var context = new ApiContext())
+            {
+                if(context.WeatherForecasts.Count()==0)
+                {
+                    context.WeatherForecasts.AddRange(weatherList);
+                    context.SaveChanges();
+                }
+
+            }
+        }
+        public List<WeatherForecast> GetWeather()
+        {
+            using (var context = new ApiContext())
+            {
+                var list = context.WeatherForecasts
+                    .ToList();
+
+                return list;
+            }
+        }
+    }
+}
